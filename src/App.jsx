1c1321b3c6ebf11f029945c51abeed17d44e581a
shakiba bahrami <shakibabahramimosaed@gmail.com>
2025-11-10 16:22:45 +0330

    project logic implemented!

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import Input from "./main.jsx";
 import { useState } from "react";
 import cities from "./cities.json";
+import { use } from "react";
 
 function App() {
   const [hint, setHint] = useState("Enter a city");
-
+  let temp = [];
   const handleChange = (event) => {
     console.log(event.target.value);
-    console.log(cities);
-    
-   //  setHint(event.target.value);
-   
-   
+    if (!event.target.value){ 
+      setHint("Enter a city")
+      return; }
+    temp = cities.filter((city) => city.startsWith(event.target.value));
+    setHint(temp[0]);
   };
 
   return <Input handleChange={handleChange} hint={hint} setHint={setHint} />;
